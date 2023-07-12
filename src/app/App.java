aa2b8a80df8b314590e2cc248aef485468e69a37
RyanBarbalho <rgtbc@ic.ufal.br>
2023-07-12 13:08:50 -0300

    adicionada classe Seller e testada no programa

--- src/app/App.java
+++ src/app/App.java
@@ -1,10 +1,17 @@
 package app;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         Department obj = new Department(1, "Books");
+
+        Seller seller = new Seller(21, "Ryan", "ryan@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
