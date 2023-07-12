e9fed88fbb32d0c2f02a262044d7cfc14a75450e
RyanBarbalho <rgtbc@ic.ufal.br>
2023-07-12 11:22:10 -0300

    criada classe Department

---
+++ src/model/entities/Department.java
@@ -0,0 +1,12 @@
+package model.entities;
+
+public class Department {
+    private Integer id;
+    private String name;
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
