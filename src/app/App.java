4aa1f3a8dd540bf3e986519c5548771fede182fc
RyanBarbalho <rgtbc@ic.ufal.br>
2023-07-13 13:07:35 -0300

    Criado classe sellerjdbc e  DaoFactory

--- src/app/App.java
+++ src/app/App.java
@@ -2,6 +2,8 @@ package app;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,9 @@ public class App {
 
         Seller seller = new Seller(21, "Ryan", "ryan@gmail.com", new Date(), 3000.0, obj);
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
+
     }
 }
